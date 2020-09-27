2a48a47edd89dd602130fc43b0a267d4dab16f01
Rowan Molony <rowanmolony@gmail.com>
2020-09-27 10:38:26 +0100

    Use django-environ for secrets
    Enables switching debug on/off from .env
    rather than from manually editing mysite/settings.py
    so git doesn't track debug config

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,11 +29,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open(f'{BASE_DIR}/etc/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env('DEBUG') # False if not in os.environ
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
